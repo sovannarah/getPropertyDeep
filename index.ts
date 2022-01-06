9c0dc0f9ef385e01cf233cb430a892760a73e19d
sovannarah <sovannara.hem@gmail.com>
2022-01-06 18:32:15 +0100

    init

--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
-function flatRecursive(a: Array<any>, searchKey: string, t: Array<any>, entityId: string) {
-    if (Array.isArray(a)) {
-        for (let i = 0; i < a.length; i++) {
-            if (a[i].profile && !entityId) {
-                entityId = a[i]._id;
-            }
-            if (typeof a[i] === "object" && !Array.isArray(a[i])) {
-                for (let [key, value] of Object.entries(a[`${i}`])) {
+function flatRecursive(data: Array<any>, searchKey: string, values: Array<any> = []) {
+    if (Array.isArray(data)) {
+        for (let i = 0; i < data.length; i++) {
+            if (typeof data[i] === "object" && !Array.isArray(data[i])) {
+                for (let [key, value] of Object.entries(data[`${i}`])) {
                     if (key === searchKey) {
                         if (value) {
                             if (Array.isArray(value) && value.length > 0) {
-                                value = value.map((x: any) => ({...x, entityId}))
+                                value = value.map((x: any) => ({...x}))
                             }
                             if (typeof value === "object" && !Array.isArray(value)) {
                                 t.push({...value});
@@ -21,11 +18,11 @@ function flatRecursive(a: Array<any>, searchKey: string, t: Array<any>, entityId
                         }
                     }
                     if (typeof value === "object" && value) {
-                        this.flatRecursive(value, searchKey, t, entityId);
+                        this.flatRecursive(value, searchKey, values);
                     }
                 }
             }
         }
     }
-    return t;
+    return values;
 }
