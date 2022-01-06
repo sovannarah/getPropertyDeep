537dd95454125195a1ae59c42d59aa56734c6d1f
sovannarah <sovannara.hem@gmail.com>
2022-01-06 18:29:48 +0100

    init

---
+++ index.ts
@@ -0,0 +1,33 @@
+function flatRecursive(a: Array<any>, searchKey: string, t: Array<any>, entityId: string) {
+    if (Array.isArray(a)) {
+        for (let i = 0; i < a.length; i++) {
+            if (a[i].profile && !entityId) {
+                entityId = a[i]._id;
+            }
+            if (typeof a[i] === "object" && !Array.isArray(a[i])) {
+                for (let [key, value] of Object.entries(a[`${i}`])) {
+                    key = key;
+
+                    if (key === searchKey) {
+                        if (value) {
+                            if (Array.isArray(value) && value.length > 0) {
+                                value = value.map((x: any) => ({...x, entityId}))
+                            }
+                            if (typeof value === "object" && !Array.isArray(value)) {
+                                t.push({...value});
+                            } else if (Array.isArray(value)) {
+                                t.push(...value);
+                            } else {
+                                t.push(value);
+                            }
+                        }
+                    }
+                    if (typeof value === "object" && value) {
+                        this.flatRecursive(value, searchKey, t, entityId);
+                    }
+                }
+            }
+        }
+    }
+    return t;
+}
